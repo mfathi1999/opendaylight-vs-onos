aa6c4acb420bca6db4069f56ce502affdfd063da
mfathi1999 <50737575+mfathi1999@users.noreply.github.com>
2023-02-03 02:14:26 +0330

    Update test_topology.py
    expand tree topology

--- test_topology.py
+++ test_topology.py
@@ -19,7 +19,14 @@ class MyTopo( Topo ):
         # Add hosts and switches
         Host1 = self.addHost( 'h1' )
         Host2 = self.addHost( 'h2' )
-        Host3 = self.addHost( 'h3' )        
+        Host3 = self.addHost( 'h3' ) 
+        Host4 = self.addHost( 'h4' )
+        Host5 = self.addHost( 'h5' )
+        Host6 = self.addHost( 'h6' ) 
+        Host7 = self.addHost( 'h7' )
+        Host8 = self.addHost( 'h8' )
+        Host9 = self.addHost( 'h9' ) 
+         
       
       
         Switch1 = self.addSwitch( 's1' )
@@ -30,8 +37,19 @@ class MyTopo( Topo ):
       
         # Add links
         self.addLink( Host1, Switch2 )
-        self.addLink( Host2, Switch3 )
-        self.addLink( Host3, Switch4 )
+        self.addLink( Host2, Switch2 )
+        self.addLink( Host3, Switch2 )
+      
+        self.addLink( Host4, Switch3 )
+        self.addLink( Host5, Switch3 )
+        self.addLink( Host6, Switch3 )
+        
+        self.addLink( Host7, Switch4 )
+        self.addLink( Host8, Switch4 )
+        self.addLink( Host9, Switch4 )
+
+        
+         
         self.addLink( Switch1, Switch2 )
         self.addLink( Switch1, Switch3 )
         self.addLink( Switch1, Switch4 )
